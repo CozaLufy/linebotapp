b0e9969c6720aff1b49c8b60880702269ba22e76
CozaLufy <science_am@hotmail.com>
2017-10-20 09:39:22 +0700

    chang callback

--- LINE-Webhook/Controllers/LineController.cs
+++ LINE-Webhook/Controllers/LineController.cs
@@ -18,105 +18,20 @@ using System.Net.Http.Headers;
 
 namespace LINE_Webhook.Controllers
 {
-    [RoutePrefix("line")]
+    [RoutePrefix("callback")]
     public class LINEController : ApiController
     {
         [HttpPost]
-        //[HttpGet]
         [Route]
         [Signature]
-        /*
-         public IHttpActionResult webhook([FromBody] LineWebhookModels data)
-         {
-             if (data == null) return BadRequest();
-             if (data.events == null) return BadRequest();
-             Trace.TraceInformation("data.events " + JsonConvert.SerializeObject(data.events));
-             foreach (Event e in data.events)
-             {
-                 if (e.type == EventType.message)
-                 {
-                     string senderID = "";
-                     switch (e.source.type)
-                     {
-                         case SourceType.user:
-                             senderID = e.source.userId;
-                             break;
-                         case SourceType.room:
-                             senderID = e.source.roomId;
-                             break;
-                         case SourceType.group:
-                             senderID = e.source.groupId;
-                             break;
-                     }
-                     Trace.WriteLine("senderID " + senderID);
-                     Trace.WriteLine("e.message.text " + e.message.text);
-                 }
-             }
-             return Ok();
-         }
-
-        public void Get()
-        {
-            string content = "{\"events\":[{\"type\":\"message\",\"replyToken\":\"568e2864472844f18c2c6c7a975f38fd\",\"source\":{\"userId\":\"U9064a2310b6a52cdbb0682912ba6179c\",\"type\":\"user\"},\"timestamp\":1508381966322,\"message\":{\"type\":\"text\",\"id\":\"6862464788030\",\"text\":\"Test\"}}]}";
-            LineWebhookModels dataEvent = JsonConvert.DeserializeObject<LineWebhookModels>(content);
-            
-            foreach (Event e in dataEvent.events)
-            {
-                if (e.type == EventType.message)
-                {
-                    ReplyBody rb = new ReplyBody()
-                    {
-                        replyToken = e.replyToken,
-                        messages = procMessage(e.message)
-                    };
-                    Trace.TraceInformation("rb " + JsonConvert.SerializeObject(rb));
-                    //Reply reply = new Reply(rb);
-                    //reply.send();
-                }
-            }
-            
-        }
-       */
+       
         public async Task<HttpResponseMessage> Post(HttpRequestMessage request)
         {
             if (request != null)
             {
                 var content = await request.Content.ReadAsStringAsync();
                 Trace.TraceInformation("request content " + content);
-                LineWebhookModels dataEvent = JsonConvert.DeserializeObject<LineWebhookModels>(content);
-                /*54.235.159.192
-                foreach (Event e in dataEvent.events)
-                {
-                    if (e.type == EventType.message)
-                    {
-                        ReplyBody rb = new ReplyBody()
-                        {
-                            replyToken = e.replyToken,
-                            messages = procMessage(e.message)
-                        };
-                        Trace.TraceInformation("rb " + JsonConvert.SerializeObject(rb));
-                        //Reply reply = new Reply(rb);
-                        //reply.send();
-
-                        var message = JsonConvert.SerializeObject(rb);
-
-                        using (var client = new HttpClient())
-                        {
-                            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", WebConfigurationManager.AppSettings["AccessToken"]);
-
-                            var dataString = new StringContent(message, Encoding.UTF8, "application/json");
-
-                            var result = await client.PostAsync("https://api.line.me/v2/bot/message/reply", dataString);
-
-                            if (!result.IsSuccessStatusCode)
-                            {
-                                throw new LineRequestException(result);
-                            }
-                        }
-                    }
-                }
-                */
+                LineWebhookModels dataEvent = JsonConvert.DeserializeObject<LineWebhookModels>(content);               
 
                 List<SendMessage> msgs = new List<SendMessage>();
                 SendMessage sm = new SendMessage();
